b21ab08d9e7b0d8da54ae36291dab179916e471a
Bennjamen Murdoch <bsmurdoc@calpoly.edu>
2026-01-13 00:28:12 +0000

    day 4 lab

--- lab1_4.py
+++ lab1_4.py
@@ -1,1 +1,11 @@
 pass
+def calculate_average(num1:int, num2:int, num3:int):
+  return (num1+num2+num3)/3
+
+
+def add_tax(bill_total): 
+  return bill_total+(bill_total*0.1)
+
+
+def greet_user(name):
+  return "Hello "+ name

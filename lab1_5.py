d83b60f857b9e2eaacb155c5baf90e749c66499a
Bennjamen Murdoch <bsmurdoc@calpoly.edu>
2026-01-13 03:15:20 +0000

    day 5 lab

--- lab1_5.py
+++ lab1_5.py
@@ -1,1 +1,24 @@
 pass
+def check_multiple(number):
+    return number % 3 == 0 and number % 5 == 0
+
+
+def check_password(input_string): 
+    password = "Python123"
+    if input_string == password:
+        return "access granted"
+    else :
+        return "access denied"
+    
+
+
+def calculate_federal_tax(salary): 
+    if salary <= 11_000 :
+        return salary*0.1
+    elif salary <= 44_725:
+        return salary* 0.12
+    elif salary <= 95_375:
+        return salary*0.22
+    else: 
+        return salary*0.24
+
